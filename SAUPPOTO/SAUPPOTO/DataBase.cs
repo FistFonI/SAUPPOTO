4e3d2df8b1bc14cf903a25e9ba39d2ff590446ba
Stepa <myasnikov.stepa11@gmail.com>
2021-12-20 13:02:39 +0400

    Добавление базовых методов

--- SAUPPOTO/SAUPPOTO/DataBase.cs
+++ SAUPPOTO/SAUPPOTO/DataBase.cs
@@ -10,5 +10,22 @@ namespace SAUPPOTO
     class DataBase
     {
         MySqlConnection connection = new MySqlConnection("server = localhost;port=3306;username=root;password=root;database=sauppotodatabase");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Open();
+        }
+        
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
     }
 }
