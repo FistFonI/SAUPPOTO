e997019b894aa34b2f6dd454261ac88d52b08d6d
Batrov D <d.batrov@yandex.ru>
2021-12-20 12:43:44 +0400

    Расписаны свойства

--- SAUPPOTO/SAUPPOTO/Salary.cs
+++ SAUPPOTO/SAUPPOTO/Salary.cs
@@ -9,15 +9,46 @@ namespace SAUPPOTO
     class Salary
     {
         private decimal hourlyPayValue;
-        public decimal HourlyPayValue;
+        public decimal HourlyPayValue
+        {
+            get { return hourlyPayValue; }
+            set
+            {
+                if (value > 0)
+                    hourlyPayValue = value;
+                else throw new ArgumentException("Размер почасовой оплаты труда должен быть больше 0.");
+            }
+        }
 
         private decimal premiumValue;
-        public decimal PremiumValue;
+        public decimal PremiumValue
+        {
+            get { return premiumValue; }
+            set
+            {
+                if (value > 0)
+                    premiumValue = value;
+                else throw new ArgumentException("Размер премии должен быть больше 0.");
+            }
+        }
 
         private int workingHours;
-        public int WorkingHours;
+        public int WorkingHours
+        {
+            get { return workingHours; }
+            set
+            {
+                if (value > 0)
+                    workingHours = value;
+                else throw new ArgumentException("Количество рабочих часов должно быть больше 0.");
+            }
+        }
 
         private decimal salaryValue;
-        public decimal SalaryValue;
+        public decimal SalaryValue
+        {
+            get { return hourlyPayValue; }
+
+        }
     }
 }
